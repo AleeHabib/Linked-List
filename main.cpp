d39c72d969960ec52f308c5b81d981e2e7c8b267
Ali Habib <alihabib.5714@gmail.com>
2024-12-22 14:20:25 +0500

    created Node

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 using namespace std;
+struct Node
+{
+    int data;  // will hold any integer value
+    int *next; // will hold the address of the next value
+};
 int main()
 {
 
