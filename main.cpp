92fbf2501983e613dc7073ffe33f1c1414facd43
Ali Habib <alihabib.5714@gmail.com>
2024-12-22 17:06:04 +0500

    fixed minor node bug

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 struct Node
 {
     int data;  // will hold any integer value
-    int *next; // will hold the address of the next value
+    Node *next; // will hold the address of the next value
 };
 int main()
 {
