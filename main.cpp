cf2f70fd8e6b3ab78230abb5ab26046e3813f7c8
Ali Habib <alihabib.5714@gmail.com>
2024-12-25 14:37:25 +0500

    added append fn

--- main.cpp
+++ main.cpp
@@ -2,9 +2,28 @@
 using namespace std;
 struct Node
 {
-    int data;  // will hold any integer value
+    int data;   // will hold any integer value
     Node *next; // will hold the address of the next value
 };
+int *append(Node *&head, int data)
+{
+    Node *new_node = new Node;
+    new_node->data = data;
+    new_node->next = nullptr;
+    if (head)
+    {
+        Node *current = head;
+        while (current)
+        {
+            current = current->next;
+        }
+        current->next = new_node;
+    }
+    else
+    {
+        head = new_node;
+    }
+}
 int main()
 {
 
